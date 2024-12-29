fce4487a9df41d08950d02eb529a4f75a3a92138
AShiSH001000 <117503374+AShiSH001000@users.noreply.github.com>
2024-12-29 15:16:02 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,7 +16,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def setup_driver():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
